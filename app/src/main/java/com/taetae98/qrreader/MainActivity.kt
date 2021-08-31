eeefa3f0d7bc259364fa524659392d88bb478f12
KangTaeJong98 <rkdxowhd98@naver.com>
2021-08-31 15:10:48 +0900

    First Commit

---
+++ app/src/main/java/com/taetae98/qrreader/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.taetae98.qrreader
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
