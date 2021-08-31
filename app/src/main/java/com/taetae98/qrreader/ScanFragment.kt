bbe45e8fdf36df0278dba41777f77f140ce5a405
KangTaeJong98 <rkdxowhd98@naver.com>
2021-08-31 17:20:52 +0900

    QR-3 ScanFragment 디자인 완성

--- app/src/main/java/com/taetae98/qrreader/ScanFragment.kt
+++ app/src/main/java/com/taetae98/qrreader/ScanFragment.kt
@@ -1,6 +1,27 @@
 package com.taetae98.qrreader
 
+import android.os.Bundle
+import android.view.*
 import com.taetae98.lib.binding.BindingFragment
 import com.taetae98.qrreader.databinding.FragmentScanBinding
 
-class ScanFragment : BindingFragment<FragmentScanBinding>(R.layout.fragment_scan)
+class ScanFragment : BindingFragment<FragmentScanBinding>(R.layout.fragment_scan) {
+    init {
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.fragment_scan_menu, menu)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        onCreateSupportActionBar()
+
+        return binding.root
+    }
+
+    private fun onCreateSupportActionBar() {
+        setSupportActionBar(binding.toolbar)
+    }
+}
